3bea20bd9d6d96257659972d734cca55bbccbb17
AsmaaAlharbi <i.sooomx@gmail.com>
2022-12-17 22:30:18 +0300

    Asmaa Commit

---
+++ src/b9_firstlab/B9_FirstLab.java
@@ -0,0 +1,14 @@
+
+package b9_firstlab;
+
+/**
+ *
+ * @author Asmaa
+ */
+public class B9_FirstLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm Asmaa Alharbi");
+    }
+    
+}
