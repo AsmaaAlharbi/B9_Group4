3e281905f576eb4424f0c39f0825ce47e2d9626a
sraaOthman <snomanothman@stu.kau.edu.sa>
2022-12-18 00:06:44 +0300

    sraa commit

--- src/b9_firstlab/B9_FirstLab.java
+++ src/b9_firstlab/B9_FirstLab.java
@@ -10,6 +10,8 @@ public class B9_FirstLab {
     public static void main(String[] args) {
         System.out.println("Hi, I'm Asmaa");
          System.out.println("Hello There!!");
+         System.out.println("Hello, I'm Sraa");
+        
     }
     
 }
