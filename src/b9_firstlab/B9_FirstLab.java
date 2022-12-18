26a6b1c31da23292d6389dc6f5f97b5b67575741
Layan <wonderlilu7@gmail.com>
2022-12-18 14:45:47 +0300

    Layan Commit

---
+++ src/b9_firstlab/B9_FirstLab.java
@@ -0,0 +1,17 @@
+
+package b9_firstlab;
+
+/**
+ *
+ * @author Asmaa
+ */
+public class B9_FirstLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm Asmaa");
+         System.out.println("Hello There!!");
+         System.out.println("Hello, I'm Sraa");
+        
+    }
+    
+}
